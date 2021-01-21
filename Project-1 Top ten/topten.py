91dcb74706b34e633150d1072f79fb5b4794667d
jisanRL <jisanlatif@gmail.com>
2021-01-20 21:38:04 -0500

    ploting works, occurance counting works, stopword stored, check by running against dracula.txt and compare stopward against dracula

--- Project-1 Top ten/topten.py
+++ Project-1 Top ten/topten.py
@@ -7,17 +7,18 @@
 # plot the top ten words of the input 
 
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plot;plot.rcdefaults()
 import os
 
-# read from stopwords.txt and push into dixtionary 
-# s = {}
-# key = ""
-# with open('stopwords-MySQL.txt') as f:
-#     for line in f:
-#         (key, val) = line.split()
-#         s[int(key)] = val
-# print(s)
+# read from stopwords.txt and push into a list 
+stopwordList = []
+with open('stopwords-MySQL.txt') as f:
+    stopwordList = [ln.strip() for ln in f]
+    # for line in f:
+    #     (key, val) = line.split()
+    #     s[int(key)] = val
+print('stopwordList=')
+print(stopwordList)
 
 # read from an input text and count occurances 
 txt = open('file.txt')
@@ -36,5 +37,27 @@ for line in txt:
 
 for key in list(dic.keys()):
     print(key, ": ", dic[key])
+    # None
+# print("Dic=" + str(dic))
+
+#plot the graph 
+xAxis = [] 
+yAxis = []
+
+for value in dic.items():
+    xAxis.append(value[0])             # get the key key of the dictionary/get the word 
+    yAxis.append(dic[value[0]])     
+print(yAxis)
+print(xAxis)
+
+yposition = np.arange(len(xAxis))
+plot.bar(yposition, yAxis, align='center', alpha=0.5)
+plot.xticks(yposition, xAxis)
+plot.ylabel('#occurances')
+plot.title('top ten usage')
+plot.show()
 
 
+# to-do
+# 1. store the stopwords.txt into dictionary/list [done]
+# 2. plot the graph 
