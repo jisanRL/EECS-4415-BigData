20ea82f324a26cef2ceaffa24417b064321774a9
jisanRL <jisanlatif@gmail.com>
2021-01-21 19:57:22 -0500

    plotting and rest done, final testing and prism testing left

--- Project-1 Top ten/topten.py
+++ Project-1 Top ten/topten.py
@@ -9,43 +9,47 @@
 import numpy as np
 import matplotlib.pyplot as plot;plot.rcdefaults()
 import os
+import sys
+import re
 
 # read from stopwords.txt and push into a list 
 stopwordList = []
 with open('stopwords-MySQL.txt') as f:
     stopwordList = [ln.strip() for ln in f]
-    # for line in f:
-    #     (key, val) = line.split()
-    #     s[int(key)] = val
 print('stopwordList=')
 print(stopwordList)
 
 # read from an input text and count occurances 
-txt = open('file.txt')
+# txt = open('file.txt')
 dic = {}
 
-for line in txt:
-    line = line.strip()     # removes space and newline
+for line in sys.stdin:
+    line = line.strip()             # removes space and newline
     line = line.lower()    
     
-    words = line.split(" ")  # splits line into words
+    # words = line.split(" ")         # splits line into words
+    words = filter(None, re.split('[\W+_]', line))  # seperating on white spaces
+    words = map(lambda a:a.lower(), words)          # converting words to lowercase
     for wrd in words:
-        if wrd in dic:      # check if word is in dic
-            dic[wrd] = dic[wrd] + 1
-        else:
-            dic[wrd] = 1
+        if wrd not in stopwordList and len(wrd) != 1:  # check if word is a stopwrod or of lenght 1
+            if wrd not in dic:                         # check if the word is in dic or not 
+                dic[wrd] = 1
+            else:
+                dic[wrd] = dic[wrd] + 1
 
-for key in list(dic.keys()):
+
+for key in list(dic.keys()):        # push the word and number of occurance in dictionary, where key=word and value=number of occurance
     print(key, ": ", dic[key])
     # None
-# print("Dic=" + str(dic))
+print("Dic=" + str(dic))
 
 #plot the graph 
 xAxis = [] 
 yAxis = []
+topTen = sorted(dic.items(), key=lambda x:-x[1])[:10]        # sorts the 10 ten 
 
-for value in dic.items():
-    xAxis.append(value[0])             # get the key key of the dictionary/get the word 
+for value in topTen:
+    xAxis.append(value[0])          # get the key key of the dictionary/get the word 
     yAxis.append(dic[value[0]])     
 print(yAxis)
 print(xAxis)
@@ -54,10 +58,12 @@ yposition = np.arange(len(xAxis))
 plot.bar(yposition, yAxis, align='center', alpha=0.5)
 plot.xticks(yposition, xAxis)
 plot.ylabel('#occurances')
-plot.title('top ten usage')
+plot.title('top ten words')
 plot.show()
 
 
 # to-do
 # 1. store the stopwords.txt into dictionary/list [done]
-# 2. plot the graph 
+# 2. plot the graph [done]
+# 3. compare dracula.txt vs stopwords.txt
+# 4. plot that one 
