3171a9e3ea4f855733be0c1dbc020ea17b7e95bf
jisanRL <jisanlatif@gmail.com>
2021-01-22 16:03:58 -0500

    all testing done

--- Project-1 Top ten/topten.py
+++ Project-1 Top ten/topten.py
@@ -1,11 +1,3 @@
-# read from the stdin
-# store them in a python dictionary 
-# each dictionaries entry value as a counter to count the instance of the word 
-# push the stopwords.txt file words into a dictionary
-# compare any random input text file vs stopwords.txt 
-# remove the stopwords in the input 
-# plot the top ten words of the input 
-
 import numpy as np
 import matplotlib.pyplot as plot;plot.rcdefaults()
 import os
@@ -27,7 +19,7 @@ for line in sys.stdin:
     line = line.strip()             # removes space and newline
     line = line.lower()    
     
-    # words = line.split(" ")         # splits line into words
+    # words = line.split(" ")                       # splits line into words
     words = filter(None, re.split('[\W+_]', line))  # seperating on white spaces
     words = map(lambda a:a.lower(), words)          # converting words to lowercase
     for wrd in words:
@@ -60,10 +52,3 @@ plot.xticks(yposition, xAxis)
 plot.ylabel('#occurances')
 plot.title('top ten words')
 plot.show()
-
-
-# to-do
-# 1. store the stopwords.txt into dictionary/list [done]
-# 2. plot the graph [done]
-# 3. compare dracula.txt vs stopwords.txt
-# 4. plot that one 
